0c4d117d1f8e5fcd07c69be158bd7ef3949f09e9
sttechshow <107465685+sttechshow@users.noreply.github.com>
2022-06-14 14:21:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94774056521']
+global.premium = ['94774056521']
+global.ownernomer = '94774056521'
+global.ownername = '🐦ST tech show'
+global.botname = 'ST tech show'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://youtube.com/c/STtechshow'
+global.packname = 'Sakeesha Tharinda'
+global.author = '🐦ST tech show'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
